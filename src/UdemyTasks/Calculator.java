9b447d6d34ac6f540702f24a7bb199f35805c837
Admin <aqilzeka99@gmail.com>
2020-02-18 08:30:51 +0400

    Initial comment

---
+++ src/UdemyTasks/Calculator.java
@@ -0,0 +1,12 @@
+package UdemyTasks;
+public class Calculator {
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet){
+        this.carpet = carpet;
+        this.floor = floor;
+    }
+
+    public double getTostalCost(){ return carpet.getCost()*floor.getArea(); }
+}
--- src/classes/Main.java
+++ src/classes/Main.java
@@ -2,9 +2,9 @@ package classes;
 
 public class Main {
     public static void main(String[] args) {
-        Car porsche = new Car(doors, wheels, engine, colour);
-        Car holden = new Car(doors, wheels, engine, colour);
-        porsche.setModel("911");
-        System.out.println("Model is " + porsche.getModel());
+    //    Car porsche = new Car(doors, wheels, engine, colour);
+     //   Car holden = new Car(doors, wheels, engine, colour);
+      //  porsche.setModel("911");
+     //   System.out.println("Model is " + porsche.getModel());
     }
 }
